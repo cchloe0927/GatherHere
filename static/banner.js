6adb1c00ce9dfdc0200bf3e8fcc8dd631939fcd6
BeomJoon <faulty337@gmail.com>
2022-10-24 18:05:55 +0900

    update banner

--- static/banner.js
+++ static/banner.js
@@ -1,8 +1,15 @@
 'use strict'
 
 $(document).ready(function () {
-    banner();
+    // banner();
+    $('#banner').load("./banner.html")
 });
+
+
+function logout(){
+    document.cookie = 'Authorization' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    location.href = '/main';
+}
 
 function banner() {
 
@@ -11,10 +18,18 @@ function banner() {
                         GatherHere
                     </a>
                     <div>
-                        <button class="header_rightBtn">로그인</button>
+                        {% if username %}
+                            {{ username }}
+                            <button class="header_rightBtn" onclick="logout()">로그아웃</button>
+                        {% endif %}
+                        {% if not username %}
+                            <button class="header_rightBtn" onclick="location.href='/login'">로그인</button>
+                            <button class="header_rightBtn">마이페이지</button>
+                        {% endif %}
                         <button class="header_rightBtn" onclick="location.href='/mypage'">마이페이지</button>
                     </div>
                 </nav>`
 
-    $('#banner').append(temp_html)
+    // $('#banner').append(temp_html)
+    $('#banner').load("/banner.html")
 }
