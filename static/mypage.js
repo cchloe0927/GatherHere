be302c29d62ae443aaf2e98215d286400de4bd41
44Chick <to44guchi@gmail.com>
2022-10-26 15:11:55 +0900

    add mypage_comment_delete

--- static/mypage.js
+++ static/mypage.js
@@ -2,9 +2,8 @@ $(document).ready(function () {
     listing_bookmark();
     listing_comment();
 });
-
-let cnt =0;
-
+let comment_count = 0;
+let b_cnt =0;
 function listing_bookmark() {
     $.ajax({
         type: 'GET',
@@ -12,37 +11,40 @@ function listing_bookmark() {
         data: {},
         success: function (response) {
             let rows = response['bookmarks']
-            for (let i = 0; i < rows.length; i++) {
-                cnt++
-                let id = rows[i]['id']
-                let title = rows[i]['title']
-                let image = rows[i]['image']
-                let star = rows[i]['star']
-                let type = rows[i]['type']
-                let creator = null
-                if (type == 'movie') {
-                    creator = rows[i]['direction']
-                } else if (type == 'book') {
-                    creator = rows[i]['author']
-                } else if (type == 'album') {
-                    creator = rows[i]['artist']
-                }
-                let temp_html = `
-                    <div class="swiper-slide" id="${id}">
-                        <div class="poster" alt="${title}" 
-                            style="background-image:url(${image})" 
-                            onclick="location.href='detail?type=${type}&id=${id}'">
-                        </div>
-                        <h4>${title}</h4>
-                        <p class="sumContent">${creator}<br>평점: ${star}</p>
-                    <div class="heart-like-button liked" href="#" id="${id}" 
-                    onclick="delete_bookmark(${id}, '${type}')"></div>
-                    </div>`
-                console.log(type)
-                $('#swipeBookmark').append(temp_html)
+            if(rows[0] != null){
+                for (let i = 0; i < rows.length; i++) {
+                    b_cnt++
+                    let id = rows[i]['id']
+                    let title = rows[i]['title']
+                    let image = rows[i]['image']
+                    let star = rows[i]['star']
+                    let type = rows[i]['type']
+                    let creator = null
+                    if (type == 'movie') {
+                        creator = rows[i]['direction']
+                    } else if (type == 'book') {
+                        creator = rows[i]['author']
+                    } else if (type == 'album') {
+                        creator = rows[i]['artist']
+                    }
+                    let temp_html = `
+                        <div id='bmk_card' class="swiper-slide" id="${id}">
+                            <div class="poster" alt="${title}" 
+                                style="background-image:url(${image})" 
+                                onclick="location.href='detail?type=${type}&id=${id}'">
+                            </div>
+                            <h4>${title}</h4>
+                            <p class="sumContent">${creator}<br>평점: ${star}</p>
+                        <div class="heart-like-button liked" href="#" id="${id}" 
+                        onclick="delete_bookmark(${id}, '${type}')"></div>
+                        </div>`
+                    console.log(type)
+                    $('#swipeBookmark').append(temp_html)
+                } console.log(b_cnt)
+                if(b_cnt) $('#bmk').hide()
+            } else {
+                $('#bmk').hide()
             }
-        },error: function () {
-            $('#bmk').hide()
         }
     })
 }
@@ -69,11 +71,10 @@ function listing_comment() {
                 let date = rows[i]['date']
 
                 let temp_html = `
-                    <div class="reviewCard_card mypage_comment">
+                    <div id="cmt_card" class="reviewCard_card mypage_comment">
                         <div>
                             <h4>${title}</h4>
                             <div>${username}님 <span>평점 : ${star_img}</span>
-
                                 <div class="delete_btn_mypage"
                                 onclick="commentDelete(${commentId})">
                                 <span class="cp_bar"></span>
@@ -85,9 +86,7 @@ function listing_comment() {
                     </div>`
                 $('#comment-list').append(temp_html)
             }
-            if(c_cnt < 1){
-                $('#cmt').hide()
-            }
+            if(c_cnt < 1) $('#cmt').hide()
         },error: function () {
             $('#cmt').hide()
         }
@@ -95,8 +94,11 @@ function listing_comment() {
 }
 
 function delete_bookmark(id, type) {
-    alert('button active')
+    b_cnt--;
     console.log(id, type)
+    if(b_cnt <= 0){
+        $('#bmk').hide();
+    }
     $.ajax({
         type: "POST",
         url: "/del_bookmark",
@@ -105,13 +107,18 @@ function delete_bookmark(id, type) {
             type: type
         },
         success: function (response) {
-            window.location.reload()
+            $('#bmk_card').remove();
         }
     });
 }
 
 
 function commentDelete(commentId) {
+    alert('button active')
+    c_cnt--;
+    if(c_cnt <=0){
+        $('#cmt').hide()
+    }
     $.ajax({
         type: "POST",
         url: "/detail/comment/delete",
@@ -119,8 +126,7 @@ function commentDelete(commentId) {
             commentId: commentId
         },
         success: function (response) {
-            alert(response["msg"])
-            window.location.reload()
+            $('#cmt_card').remove()
         }
     });
 }
