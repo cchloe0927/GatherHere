11c6726d9af19d0f004f0131a57d62b812d955dd
BeomJoon <faulty337@gmail.com>
2022-10-28 17:32:32 +0900

    update user

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ from config import SECRET_KEY, CLIENT_ID, REDIRECT_URI, LOGOUT_REDIRECT_URI
 from Oauth import Oauth
 from models.User import User
 
-import test, bookmark
+import test, bookmark, user
 
 app = Flask(__name__)
 app.register_blueprint(test.bp) #이런식으로 등록
-app.register_blueprint(bookmark) #즐겨찾기
+app.register_blueprint(bookmark.bp) #즐겨찾기
+app.register_blueprint(user.bp)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
